89677d73e86f17f91144bfb73e168c0954ef86f7
Jurandir Brito Ferreira Junior <jurandir.junior@termaco.com.br>
2021-08-09 17:43:49 -0300

    Ajuste endpoint

--- src/controllers/appCliente/listDadosCTRC.js
+++ src/controllers/appCliente/listDadosCTRC.js
@@ -7,8 +7,11 @@ const json2xlsx    = require('json2xls')
 const crypto       = require('crypto')
 const fs           = require('fs')
 
-const URL_DOWNLOAD    = `http://201.49.34.12:5000//downloads`
-const QTDE_LINHAS_XLS = 600
+const server          = process.env.SERVER || 'localhost'
+const port            = process.env.PORT   || '4999'
+
+const URL_DOWNLOAD    = `http://${server}:${port}/downloads`
+const QTDE_LINHAS_XLS = 800
 
 async function listDadosCTRC( req, res ) {
     let userId_Token = `${req.userId}`
@@ -22,7 +25,7 @@ async function listDadosCTRC( req, res ) {
         length: 0,
     }
     
-    let {dt_inicial,dt_final,DadosOuXlsx,ctrc,pagina_nro,pagina_tam} = req.query
+    let {Base,dt_inicial,dt_final,DadosOuXlsx,ctrc,pagina_nro,pagina_tam} = req.query
     let cnpj         = userId_Token
     let s_where      = ''
 	let	i_ctrc       = 0
@@ -34,6 +37,10 @@ async function listDadosCTRC( req, res ) {
         resposta.message = 'Problemas com a autenticação !!!'
         res.json(resposta).status(200)
         return 0
+    }
+
+    if(!Base) {
+        Base = 'softran_termaco'
     }
 
     if(!pagina_nro || pagina_nro==undefined ) {
@@ -49,90 +56,97 @@ async function listDadosCTRC( req, res ) {
 		i_ctrc       = `${ctrc}`.substr(4,10)
 		s_emp        = `${ctrc}`.substr(0,3)
 		s_ctrc_serie = `${ctrc}`.substr(3,1)
-        s_where2     = ` AND CNH.CTRC = ${i_ctrc} AND CNH.EMP_CODIGO='${s_emp}' AND CNH.SERIE='${s_ctrc_serie}' `
+        s_where2     = ` AND CNH.NrDoctoFiscal = ${i_ctrc} AND EMP.DsApelido='${s_emp}' `
     } 
 
-
     if(dt_inicial) {
-            s_where = s_where + ` AND CNH.DATA  >= '${dt_inicial}' `
+            s_where = s_where + ` AND CNH.DtEmissao  >= '${dt_inicial}' `
     }
 
     if(dt_final) {
-            s_where = s_where + ` AND CNH.DATA  <= '${dt_final}' `
+            s_where = s_where + ` AND CNH.DtEmissao  <= '${dt_final}' `
     }
 
     resposta.page   = Number.parseInt(pagina_nro)
     resposta.length = Number.parseInt(pagina_tam)
 
     let sql_base =`SELECT 
-                    CNH.EMP_CODIGO                as "Empresa",
-                    ISNULL(CNH.CTRCREDESP,0)      as "CTRC Origem",
-                    CNH.CTRC                      as "CTRC",
-                    CNH.DATA                      as "Data Emissão",
-                    CNH.DATAENTREGA               as "Data Entrega",
-                    CNH.DATACOLETA                as "Data Coleta",
-                    CNH.DATAEMBARQUE              as "Data Embarque",
-                    CNH.DATACHEGADA               as "Data Chegada",
-                    CNH.PREVENTREGA               as "Previsão de Entrega",
-                    CNH.CLI_CGCCPF_REMET          as "CNPJ Remetente",
-                    REM.NOME                      as "Remetente",
-                    CNH.CLI_CGCCPF_DEST           as "CNPJ Destinatário",
-                    DES.NOME                      as "Destinatário",
-                    CNH.CLI_CGCCPF_PAG            as "CNPJ Pagador",
-                    PAG.NOME                      as "Pagador",
-                    CNH.PNF                       as "Peso NF",
-                    CNH.PCALC                     as "Peso Calculado",
-                    ISNULL(CNH.PCUBADO,0)         as "Peso Cubado",
-                    CNH.PCALC                     as "Peso Cubado Cliente",
-                    CNH.VOLUME                    as "Volumes",
-                    CNH.VALORNF                   as "Valor NF",
-                    CNH.FPESO                     as "Frete Peso",
-                    CNH.FVALOR                    as "Frete Valor",
-                    CNH.COLETA                    as "Valor Coleta",
-                    CNH.VALORGRIS                 as "Valor GRIS",
-                    CNH.ENTREGA                   as "Valor Entrega",
-                    CNH.DESPACHO                  as "Valor Despacho",
-                    CNH.OUTROS                    as "Valor Outros",
-                    CNH.PEDAGIO                   as "Valor Pedagio",
-                    CNH.ALIQIMP                   as "Alíquota ICMS",
-                    CNH.ICMS                      as "ICMS",
-                    CNH.BASIMPCALC                as "Base Calc ICMS",
-                    CNH.VALORTRT                  as "Valor TRT",
-                    CNH.TOTFRETE                  as "Total do Frete",
-                    CNH.NF                        as "ListaNF",
-                    CONCAT(COL.NOME,' - ',COL.UF) as "Local de Coleta",
-                    CONCAT(ENT.NOME,' - ',ENT.UF) as "Local de Entrega",
-                    ENT.UF                        as "Destinatário UF",
-                    ENT.NOME                      as "Destinatário Cidade",
-                    DES.BAIRRO                    as "Destinatário Bairro",
-                    (SELECT TOP 1 OUN.OCO_CODIGO
-                        FROM CARGASSQL.dbo.OUN
-                        JOIN CARGASSQL.dbo.OCO ON OCO.CODIGO=OUN.OCO_CODIGO  
-                        WHERE OUN.TABELA='CNH' AND OUN.CHAVE=CONCAT(CNH.EMP_CODIGO,SERIE,CNH.CTRC) AND OCO.NAOENVIAEDI=0
-                        ORDER BY DATA DESC)          as "Código Ocorrência",
-                        (SELECT TOP 1 OUN.DESCRICAO
-                        FROM CARGASSQL.dbo.OUN
-                        JOIN CARGASSQL.dbo.OCO ON OCO.CODIGO=OUN.OCO_CODIGO  
-                        WHERE OUN.TABELA='CNH' AND OUN.CHAVE=CONCAT(CNH.EMP_CODIGO,SERIE,CNH.CTRC) AND OCO.NAOENVIAEDI=0
-                        ORDER BY DATA DESC)          as "Descrição Ocorrência",
-                    IME.RECEBEDOR                 as "Recebedor Entrega",
-                    CNH.CHAVECTE                  as "Chave CT-e"
-                FROM CARGASSQL.dbo.CNH
-                JOIN CARGASSQL.dbo.CLI REM  ON REM.CGCCPF = CNH.CLI_CGCCPF_REMET
-                JOIN CARGASSQL.dbo.CLI DES  ON DES.CGCCPF = CNH.CLI_CGCCPF_DEST
-                JOIN CARGASSQL.dbo.CLI PAG  ON PAG.CGCCPF = CNH.CLI_CGCCPF_PAG
-                JOIN CARGASSQL.dbo.CID COL  ON COL.CODIGO = REM.CID_CODIGO
-                JOIN CARGASSQL.dbo.CID ENT  ON ENT.CODIGO = DES.CID_CODIGO
-                LEFT JOIN CARGASSQL.dbo.IME ON IME.EMP_CODIGO_CNH=CNH.EMP_CODIGO AND IME.CNH_SERIE=CNH.SERIE AND IME.CNH_CTRC = CNH.CTRC
-                WHERE 
-                    (
-                    ( SUBSTRING(cnh.cli_cgccpf_remet,1,8) = ${wraiz}) OR 
-                    ( SUBSTRING(cnh.cli_cgccpf_dest,1,8)  = ${wraiz})  OR 
-                    ( SUBSTRING(cnh.cli_cgccpf_pag,1,8)   = ${wraiz}) 
-                    )                
-                    ${s_where}
-                    ${s_where2}
-                ORDER BY CNH.DATA
+                      EMP.dsapelido                         as "Empresa"
+                ,     ISNULL(CNH.NrConhecRedespacho,0)      as "CTRC Origem"
+                ,     CNH.nrdoctofiscal                     as "CTRC"
+                ,     MAN.NrManifesto                       as "Manifesto"
+                ,     CNH.DtEmissao                         as "Data Emissão"
+                ,     CNH.DtEntrega                         as "Data Entrega"
+                ,     COL.DtCadastro                        as "Data Coleta"
+                ,     MAN.DtSaida                           as "Data Embarque"    -- DATAHORA EMBARQUE - SAIDA DO DEPOSITO - FILIAL ORIGEM
+                ,     MAN.DtChegada                         as "Data Chegada"     -- DATAHORA CHEGADA  - CHEGADA NA FILIAL DESTINO
+                ,	  ${Base}.dbo.SP_CalculaDtPrevisaoEntregaPercurso(CNH.DtEmissao, CNH.CdEmpresaDestino, CNH.CdPercurso, CNH.CdTransporte, CNH.CdRemetente, CNH.CdDestinatario, CNH.cdempresa, CNH.nrseqcontrole) 
+                                                            as "Previsão de Entrega"
+                ,    CNH.CdRemetente                        as "CNPJ Remetente"
+                ,    REM.dsentidade                         as "Remetente"
+                ,    CNH.CdDestinatario                     as "CNPJ Destinatário"
+                ,    DES.dsentidade                         as "Destinatário"
+                ,    CNH.CdInscricao                        as "CNPJ Pagador"
+                ,    PAG.dsentidade                         as "Pagador"
+                ,    CNH.QtPeso                             as "Peso NF"                -- PESO TOTAL DAS NFS NO CONHECIMENTO
+                ,    CNH.QtPeso                             as "Peso Calculado"         -- PESO DO CONHECIMENTO
+                ,    ISNULL(CNH.QtPesoCubado,0)             as "Peso Cubado"           
+                ,    ISNULL(CNH.QtPesoCubado,0)             as "Peso Cubado Cliente"
+                ,    CNH.QtVolume                           as "Volumes"
+                ,    CNH.VlMercadoria                       as "Valor NF"
+                ,    CNH.VlFretePeso                        as "Frete Peso"
+                ,    CNH.VlFreteValor                       as "Frete Valor"
+                ,    CNH.VlSECCAT                           as "Valor Coleta"
+                ,    CNH.VlGris                             as "Valor GRIS" 
+                ,    CNH.VlTDE                              as "Valor Entrega"
+                ,    CNH.VlDespacho                         as "Valor Despacho"
+                ,    CNH.VlOutros                           as "Valor Outros"
+                ,    CNH.VlPedagio                          as "Valor Pedagio"
+                ,    CNH.VlAliqICMS                         as "Alíquota ICMS"
+                ,    CNH.VlICMS                             as "ICMS"
+                ,    CNH.VlBaseCalculo                      as "Base Calc ICMS"
+                ,    CNH.VlITR                              as "Valor TRT"
+                ,    CNH.VlTotalPrestacao                   as "Total do Frete"
+                ,    CNH.NrNotaFiscal                       as "ListaNF"               -- STRING COM RELAÇÃO DAS NFS
+                ,    CONCAT(COL.DsLocal,' - ',COL.DsUF)     as "Local de Coleta"
+                ,    CONCAT(ENT.DsLocal,' - ',ENT.DsUF)     as "Local de Entrega"
+                ,    ENT.DsUF                               as "Destinatário UF"
+                ,    ENT.DsLocal                            as "Destinatário Cidade"
+                ,    DES.DsBairro                           as "Destinatário Bairro"
+                ,    (SELECT TOP 1 OUN.CdOcorrencia FROM ${Base}.dbo.gtcmoven OUN 
+                    JOIN dbo.gtchisen OCO  ON OCO.cdhistoricoentrega = OUN.cdocorrencia     
+                    WHERE isnull(OCO.InExibehist, 0) = 0
+                    AND OUN.cdempresa = CNH.CdEmpresa   AND OUN.nrseqcontrole = CNH.NrSeqControle
+                    ORDER BY OUN.CdSequencia DESC )      as "Código Ocorrência"
+                ,    (SELECT TOP 1 OCO.DsHistoricoEntrega FROM ${Base}.dbo.gtcmoven OUN 
+                    JOIN dbo.gtchisen OCO  ON OCO.cdhistoricoentrega = OUN.cdocorrencia     
+                    WHERE isnull(OCO.InExibehist, 0) = 0
+                    AND OUN.cdempresa = CNH.CdEmpresa   AND OUN.nrseqcontrole = CNH.NrSeqControle
+                    ORDER BY OUN.CdSequencia DESC )      as "Descrição Ocorrência"
+                ,     (SELECT TOP 1 ENT.DsContato FROM ${Base}.dbo.GTCMOVEN ENT 
+                    WHERE ENT.CdEmpresa = CNH.CdEmpresa AND ENT.NrSeqControle = CNH.NrSeqControle 
+                    AND ENT.CdOcorrencia=1)           as "Recebedor Entrega"
+                ,     CNF.CdChaveCTe                       as "Chave CT-e"
+                ,CNH.CdEmpresa
+                ,CNH.NrSeqControle
+                FROM      ${Base}.dbo.gtcconhe CNH
+                     JOIN ${Base}.dbo.sisempre EMP  ON EMP.CdEmpresa      = CNH.CdEmpresa
+                     JOIN ${Base}.dbo.siscli   REM  ON REM.CdInscricao    = CNH.Cdremetente
+                     JOIN ${Base}.dbo.siscli   DES  ON DES.CdInscricao    = CNH.CdDestinatario
+                     JOIN ${Base}.dbo.siscli   PAG  ON PAG.CdInscricao    = CNH.CdInscricao
+                     JOIN ${Base}.dbo.siscep   COL  ON COL.NrCep          = CNH.NrCepColeta
+                     JOIN ${Base}.dbo.siscep   ENT  ON ENT.NrCep          = CNH.NrCepEntrega
+                LEFT JOIN ${Base}.dbo.gtcconce CNF  ON CNF.cdempresa      = CNH.cdempresa  AND CNF.nrseqcontrole = CNH.nrseqcontrole
+                LEFT JOIN ${Base}.dbo.CCEColet CLT  ON CLT.CdEmpresa      = CNH.CdEmpresa  AND CLT.NrColeta      = CNH.NrColeta
+                LEFT JOIN ${Base}.dbo.GTCManCn MAC  ON MAC.CdEmpresa      = CNH.CdEmpresa  AND MAC.NrSeqControle = CNH.NrSeqControle
+                LEFT JOIN ${Base}.dbo.GTCMAN   MAN  ON MAN.NrManifesto    = MAC.NrManifesto			
+                WHERE         
+                    (SUBSTRING(CNH.cdremetente,1,8)      = '${wraiz}' OR 
+                     SUBSTRING(CNH.cddestinatario,1,8)   = '${wraiz}' OR 
+                     SUBSTRING(CNH.cdinscricao,1,8)      = '${wraiz}'    )
+                     ${s_where}                     
+                     ${s_where2}
+                ORDER BY CNH.DtEmissao
                 OFFSET (${pagina_nro} - 1) * ${pagina_tam} ROWS
                 FETCH NEXT ${pagina_tam} ROWS ONLY `                
 
@@ -142,16 +156,24 @@ async function listDadosCTRC( req, res ) {
     // console.log('(listDadosCTRC)  API => SQL:',s_select)
         
     try {  
+        
+        console.log('SQL:',s_select)
+
         const result = await sqlQuery(s_select)
 
-            if (err) {
+        console.log('result:',result)
+
+            if (result.Erro) {
+                
                 resposta.success = false
-                resposta.message = `ERRO: ${err}`  
+                resposta.message = `ERRO: ${result.Erro}`  
+                res.json(resposta).status(500)
+
             } else {  
                 let xlsx 
                 let filename 
                 let dados = []
-                dados.push(...profileset.recordset)
+                dados.push(...result)
                 resposta.data     = dados
                 resposta.rows     = dados.length
                 resposta.success  = (resposta.rows>0)
